17773c437a5fa6477f87ce8604ecb9c5c0d9c409
Timothy Pokanai <pokanait@mcmaster.ca>
2026-01-14 15:23:42 +0100

    Fixed command line arguments for lowercase, stem. Fixed empty tokens appearing, and lowercase not actually working

--- normalize_text.py
+++ normalize_text.py
@@ -5,44 +5,39 @@ from collections import Counter
 from nltk.stem import PorterStemmer
 
 parser = argparse.ArgumentParser()
-parser.add_argument(
-    "normalizations", 
-    nargs="*",
-    choices=["lowercase", "stem"],
-    help="Token normalization options: lowercase, stem"
-    )
+
 parser.add_argument("filename", help="The path to the input plain text file")
+parser.add_argument("-lowercase", action="store_true", help="Lowercase normalization")
+parser.add_argument("-stem", action="store_true", help="Stemming normalization")
 
 args = parser.parse_args()
 
 stem = PorterStemmer()
 
 def tokenize(plain_text):
-    tokens = re.split(r'\W+', plain_text)
-    return tokens
+    return [t for t in re.split(r"\W+", plain_text) if t]
 
-def normalize(tokens, normalizations):
-    for norm_type in normalizations:
-        if norm_type =="lowercase":
-            tokens = [t.lower() for t in tokens]
-        elif norm_type =="stem":
-            tokens = [stem.stem(t) for t in tokens]
+def normalize(tokens, args):
+    if args.lowercase:
+        tokens = [t.lower() for t in tokens]
+    if args.stem:
+        tokens = [stem.stem(t) for t in tokens]
     return tokens
 
 def count_tokens(tokens):
     return Counter(tokens).most_common()
 
-def process_file(filename, normalizations):
+def process_file(filename, args):
     try:
-        with open(args.filename, 'r', encoding='utf-8') as file:
+        with open(filename, 'r', encoding='utf-8') as file:
             text = file.read()
 
             tokens = tokenize(text)
-            tokens = normalize(tokens, normalizations)
+            normalized_tokens = normalize(tokens, args)
 
-            return count_tokens(tokens)
+            return count_tokens(normalized_tokens)
     except FileNotFoundError:
         print(f"Error: File '{filename}' not found.")
 
-tokens_counts = process_file(args.filename, args.normalizations)
+tokens_counts = process_file(args.filename, args)
 print(tokens_counts[0:25])
